a311ac02388e659193cdd8ee9826ef4ce77197a7
VitaLeek <vitaleek.bliz@gmail.com>
2026-02-13 10:11:01 +0200

    fix: fixed private variables

--- app/services/lot/lot.py
+++ app/services/lot/lot.py
@@ -10,39 +10,46 @@ class Lot:
         self.id = self.static_id_couter
         self.static_id_counter+=1
 
-        self.lifeduration = start_lifeduration
-        self.lifeduration_on_update = lifeduration_on_update
+        self._lifeduration = start_lifeduration
+        self._lifeduration_on_update = lifeduration_on_update
 
-        self.current_price = starting_price
-        self.bid_step = bid_step
+        self._current_price = starting_price
+        self._bid_step = bid_step
 
-        self.start_time = datetime.now(timezone.utc)
-        self.end_time = datetime.now(timezone.utc).replace(second=self.timer_start.second + self.lifeduration)
+        self._start_time = datetime.now(timezone.utc)
+        self._end_time = datetime.now(timezone.utc).replace(second=self.timer_start.second + self._lifeduration)
 
         self.is_closed = False
 
     def bid(self, new_bid:int):
+        """
+        Place bid by user
+        """
         if(self.check_status == LotStatus.ENDED):
-            raise AuctionEndedError(f"Auction ended at {self.end_time}")
+            raise AuctionEndedError(f"Auction ended at {self._end_time}")
         
-        new_price = new_bid + self.bid_step
-        if(self.current_price <= new_price):
-            raise AuctionBidTooLowError(f"Auction price : {self.current_price}, step : {self.bid_step}; current bid price : {new_price}")
+        new_price = new_bid + self._bid_step
+        if(self._current_price <= new_price):
+            raise AuctionBidTooLowError(f"Auction price : {self._current_price}, step : {self._bid_step}; current bid price : {new_price}")
 
         self._update_price(new_price)
 
     def _update_price(self, new_val:int):
-        self.current_price = new_val
+        """
+        Private function. Should do checks before running the funcion.
+        """
+        self._current_price = new_val
 
         #update lifetime
         #TODO Ps. very high bid difference (in %) can have short lifetime
         now = datetime.now(timezone.utc)
-        self.end_time = now.replace(second=self.now.second + self.lifeduration_on_update)
+        self._end_time = now.replace(second=self.now.second + self._lifeduration_on_update)
 
     def check_status(self) -> LotStatus:
-        """Динамічно обчислюємо статус на основі часу"""
-        if datetime.now(timezone.utc) >= self.end_time:
+        """Calculate status and close lot is ran out of time"""
+        if self.is_closed or datetime.now(timezone.utc) >= self._end_time:
             self.is_closed = True
             return LotStatus.ENDED
         
         return LotStatus.RUNNING
+
